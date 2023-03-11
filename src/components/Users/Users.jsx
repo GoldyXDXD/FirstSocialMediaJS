5449fddbb6d2c27eb156a17555c54a60034999ac
GoldyXDXD <teamnescafe@yandex.ru>
2023-03-11 15:15:17 +0300

    Убраны встроенные пользователи (теперь через API генерируются), добавлены классовые компоненты (потом будут удалены, написаны чисто для обучения)

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,80 +1,52 @@
-import styles from './users.module.css';
+import userPhoto from "../../logo192.png";
+import styles from "./users.module.css";
+import axios from "axios";
+import {Component} from "react";
 
-let Users = (props) => {
-    if (props.users.length === 0) {
-        props.setUsers([
-                {
-                    id: 1,
-                    photoUrl: "https://kartinkin.net/pics/uploads/posts/2022-08/1660363378_18-kartinkin-net-p-valli-oboi-na-rabochii-stol-krasivo-19.jpg",
-                    followed: true,
-                    fullName: "Nick",
-                    status: 'A YA NIKITA',
-                    location: {city: 'Balashikha', country: "Russia"}
-                },
-                {
-                    id: 2,
-                    photoUrl: "https://disney-planet.fr/wp-content/uploads/2010/04/m-o-personnage-wall-e-05.jpg",
-                    followed: false,
-                    fullName: "Katya",
-                    status: 'Solo 322',
-                    location: {city: 'Moscow', country: "Russia"}
-                },
-                {
-                    id: 3,
-                    photoUrl: "https://a.d-cd.net/B0AAAgN1-OA-1920.jpg",
-                    followed: true,
-                    fullName: "Mikhail",
-                    status: 'qwerty',
-                    location: {city: 'Moscow', country: "Russia"}
-                },
-                {
-                    id: 4,
-                    photoUrl: "https://kg-portal.ru/img/100899/main_2x.jpg",
-                    followed: true,
-                    fullName: "Evgeniy",
-                    status: 'pudge',
-                    location: {city: 'Zheleznodorozhniy', country: "Russia"}
-                },
-                {
-                    id: 5,
-                    photoUrl: "https://kartinkin.net/uploads/posts/2022-10/1665534097_3-kartinkin-net-p-chelovek-benzopila-pauer-art-instagram-3.jpg",
-                    followed: false,
-                    fullName: "Svyat",
-                    status: 'privet medved',
-                    location: {city: 'Belgrade', country: "Serbia"}
-                }
-            ]
-        )
+class Users extends Component{
+
+    constructor(props) {
+        super(props);
+
+        axios.get("https://social-network.samuraijs.com/api/1.0/users")
+            .then(response => {
+                this.props.setUsers(response.data.items);
+            })
     }
-    return <div>
-        {
-            props.users.map(u => <div key={u.id}>
+
+    render() {
+        return <div>
+            {
+                this.props.users.map(u => <div key={u.id}>
                 <span>
                     <div>
-                        <img src={u.photoUrl} className={styles.userPhoto}/>
+                        <img src={u.photos.small != null ? u.photos.small : userPhoto} className={styles.userPhoto}/>
                     </div>
                     <div>
                         {u.followed
                             ? <button onClick={() => {
-                                props.unfollow(u.id)
+                                this.props.unfollow(u.id)
                             }}>Unfollow</button>
                             : <button onClick={() => {
-                                props.follow(u.id)
+                                this.props.follow(u.id)
                             }}>Follow</button>
                         }
                     </div>
                 </span>
-                <span>
                     <span>
-                        <div>{u.fullName}</div><div>{u.status}</div>
+                    <span>
+                        <div>{u.name}</div>
+                        <div>{u.status}</div>
                     </span>
                     <span>
-                        <div>{u.location.country}</div>
-                        <div>{u.location.city}</div>
+                        <div>{"u.location.country"}</div>
+                        <div>{"u.location.city"}</div>
                     </span>
                 </span>
-            </div>)
-        }
-    </div>
+                </div>)
+            }
+        </div>
+    }
 }
+
 export default Users;
--- src/redux/UsersReducer.js
+++ src/redux/UsersReducer.js
@@ -3,13 +3,7 @@ const UNFOLLOW = 'UNFOLLOW';
 const SET_USERS = 'SET_USERS';
 
 let initialState = {
-    users: [
-        {id: 1, photoUrl: "https://kartinkin.net/pics/uploads/posts/2022-08/1660363378_18-kartinkin-net-p-valli-oboi-na-rabochii-stol-krasivo-19.jpg",followed: true, fullName: "Nick", status: 'A YA NIKITA', location: {city: 'Balashikha', country: "Russia"}},
-        {id: 2, photoUrl: "https://disney-planet.fr/wp-content/uploads/2010/04/m-o-personnage-wall-e-05.jpg",followed: false, fullName: "Katya", status: 'Solo 322', location: {city: 'Moscow', country: "Russia"}},
-        {id: 3, photoUrl: "https://a.d-cd.net/B0AAAgN1-OA-1920.jpg",followed: true, fullName: "Mikhail", status: 'qwerty', location: {city: 'Moscow', country: "Russia"}},
-        {id: 4, photoUrl: "https://kg-portal.ru/img/100899/main_2x.jpg",followed: true, fullName: "Evgeniy", status: 'pudge', location: {city: 'Zheleznodorozhniy', country: "Russia"}},
-        {id: 5, photoUrl: "https://kartinkin.net/uploads/posts/2022-10/1665534097_3-kartinkin-net-p-chelovek-benzopila-pauer-art-instagram-3.jpg",followed: false, fullName: "Svyat", status: 'privet medved', location: {city: 'Belgrade', country: "Serbia"}}
-    ]
+    users: []
 }
 
 const usersReducer = (state = initialState, action) => {
